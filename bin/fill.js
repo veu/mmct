f738a0137c110732fa39cbf99c7f914baf27ad8f
Becky <becky@monometric.net>
2017-04-17 07:37:07 +0200

    inline commands

--- bin/fill.js
+++ bin/fill.js
@@ -1,13 +1,33 @@
 #!/usr/bin/env node
 
-const commands = require('../src/commands');
+const contentful = require('../src/contentful');
+const entryWriter = require('../src/entry-writer');
+const getStdin = require('get-stdin');
 const yargs = require('yargs');
+
+const reportError = (error) => {
+    try {
+        error = JSON.parse(error.message);
+    } catch (ignore) {}
+
+    console.error('Error: ' + error.message);
+};
 
 yargs
     .version()
     .usage('$0 <cmd> [args] [options]')
     .command('default-value <space> <token> <content-model-id> <field>', 'enter a default value where missing', {}, async function (argv) {
-        await commands.fillDefaultValue(argv.space, argv.token, argv.contentModelId, argv.field);
+        try {
+            console.log('Reading from stdin…');
+            const value = await getStdin();
+
+            const space = await contentful.getSpace(argv.space, argv.token);
+            const stats = await entryWriter.fillDefaultValue(space, argv.contentModelId, argv.field, value);
+
+            console.log(`Updated ${stats.updatedCount} entries.`);
+        } catch (e) {
+            reportError(e);
+        }
     })
     .demandCommand(1, 'Please provide a valid command.')
     .help()
--- bin/trim.js
+++ bin/trim.js
@@ -1,7 +1,17 @@
 #!/usr/bin/env node
 
-const commands = require('../src/commands');
+const contentful = require('../src/contentful');
+const orphanedAssetTrimmer = require('../src/orphaned-asset-trimmer');
+const outdatedEntryTrimmer = require('../src/outdated-entry-trimmer');
 const yargs = require('yargs');
+
+const reportError = (error) => {
+    try {
+        error = JSON.parse(error.message);
+    } catch (ignore) {}
+
+    console.error('Error: ' + error.message);
+};
 
 yargs
     .version()
@@ -18,10 +28,30 @@ yargs
         default: 5
     })
     .command('orphaned-assets <space> <token>', 'delete unused assets', {}, async function (argv) {
-        await commands.trimOrphanedAssets(argv.space, argv.token, argv.gracePeriod, argv.dryRun);
+        contentful.config.gracePeriod = argv.gracePeriod;
+        contentful.config.isDryRun = argv.dryRun;
+
+        try {
+            const space = await contentful.getSpace(argv.space, argv.token);
+            const stats = await orphanedAssetTrimmer.trim(space);
+
+            console.log(`Deleted ${stats.deletedCount} orphaned assets.`);
+        } catch (e) {
+            reportError(e);
+        }
     })
     .command('outdated-entries <space> <token> <field>', 'delete entries where <field> is in the past', {}, async function (argv) {
-        await commands.trimOutdatedEntries(argv.space, argv.token, argv.field, argv.gracePeriod, argv.dryRun);
+        contentful.config.gracePeriod = argv.gracePeriod;
+        contentful.config.isDryRun = argv.dryRun;
+
+        try {
+            const space = await contentful.getSpace(argv.space, argv.token);
+            const stats = await outdatedEntryTrimmer.trim(space, argv.field);
+
+            console.log(`Deleted ${stats.deletedCount} outdated entries.`);
+        } catch (e) {
+            reportError(e);
+        }
     })
     .demandCommand(1, 'Please provide a valid command.')
     .help()
